91b706dd20cb8cd24cb415d9c9106e1c7802dbd2
gammacho <joungmin.cho@gmail.com>
2025-05-06 20:50:08 +0900

    debug

--- keyboards/totem/keymaps/gammacho_38_v1/keymap.c
+++ keyboards/totem/keymaps/gammacho_38_v1/keymap.c
@@ -230,7 +230,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case MAKE_H:
             if (record->event.pressed) {
-                SEND_STRING("qmk compile -kb geigeigeist/totem -km default");
+                SEND_STRING("qmk compile -kb totem -km gammacho_38_v1");
                 tap_code(KC_ENTER);
             }
             break;
@@ -434,7 +434,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         case CO_LANG:
             if (pressed){
                 if (state == _MAC) {
-                    tap_code16_delay(LCTL(KC_SPC), 5);
+                    tap_code16(LCTL(KC_SPC));
                 }
                 else if (state == _WINDOWS) {
                     tap_code16(KC_RALT);
@@ -496,7 +496,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_WINDOWS] = LAYOUT(
                  WFNC_Q,  KC_W, KC_E,    KC_R,    KC_T,                          KC_Y,      KC_U,    KC_I,    KC_O,   WFNC_P,
                  MOU_A,   KC_S, KC_D,    LSFT_F,  KC_G,                          KC_H,      RSFT_J,  KC_K,    KC_L,   NAV_QUO,
-        KC_LSFT, HO_LSFT, KC_X, KC_C,    KC_V,    KC_B,                          KC_N,      KC_M,    KC_COMM, KC_DOT, HO_RSFT, KC_RSFT,
+        KC_LSFT, HO_LSFT, KC_X, KC_C,    KC_V,    KC_B,                          KC_N,      KC_M,    KC_COMM, KC_DOT, HO_RSFT, XXXXXXX,
                                          KC_LALT, KC_LCTL, WNUM_SP,     WNUM_SP, TD(TD_SP), KC_LGUI
     ),
 
@@ -510,43 +510,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT(
                  CC_EXLM, CC_AT,   CC_HASH, KC_EQL,  TD(TD_LBK),                       TD(TD_RBK), XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX,
                  CC_PERC, CC_AMPR, CC_ASTR, KC_SLSH, TD(TD_LPR),                       TD(TD_RPR), KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,
-        KC_LSFT, NV_LSFT, CC_DLR,  CC_PLUS, KC_MINS, CC_PIPE,                          KC_BSLS,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+        KC_LSFT, CC_HASH, CC_DLR,  CC_PLUS, KC_MINS, CC_PIPE,                          KC_BSLS,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                             _______, _______,    _______,     _______, _______, _______
     ),
 
     [_MOUSE] = LAYOUT(
                  XXXXXXX, XXXXXXX, KC_WH_U, XXXXXXX, KC_PGUP,                       KC_HOME, XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX,
                  XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_R, KC_PGDN,                       KC_END,  KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,
-        KC_LSFT, XXXXXXX, XXXXXXX, MAKE_H,  XXXXXXX, CC_PIPE,                       KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
+        KC_LSFT, XXXXXXX, XXXXXXX, MAKE_H,  XXXXXXX, CC_PIPE,                       KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                             KC_LEFT, KC_RGHT, _______,     _______, _______, _______
     ),
 
     [_M_FNC] = LAYOUT(
                  XXXXXXX, KC_VOLU, XXXXXXX, LGUI(KC_LBRC), LGUI(KC_RBRC),                            LCTL(KC_UP),  KC_F9, KC_F10, KC_F11, KC_F12,
                  XXXXXXX, KC_VOLD, XXXXXXX, XXXXXXX,       LGUI(KC_GRV),                             LGUI(KC_GRV), KC_F5, KC_F6,  KC_F7,  KC_F8,
-        KC_LSFT, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX,       LGUI(LALT(KC_ESC)),                       MSNAP,        KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_RSFT,
+        KC_LSFT, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX,       LGUI(LALT(KC_ESC)),                       MSNAP,        KC_F1, KC_F2,  KC_F3,  KC_F4,  XXXXXXX,
                                             _______,       _______,            _______,     _______, _______,      _______
     ),
 
     [_W_FNC] = LAYOUT(
                  XXXXXXX, KC_VOLU, XXXXXXX, LALT(KC_LEFT), LALT(KC_RGHT),                            LGUI(KC_TAB), KC_F9, KC_F10, KC_F11, KC_F12,
                  XXXXXXX, KC_VOLD, XXXXXXX, XXXXXXX,       LALT(KC_TAB),                             LALT(KC_TAB), KC_F5, KC_F6,  KC_F7,  KC_F8,
-        KC_LSFT, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX,       LCTL(LSFT(KC_ESC)),                       WSNAP,        KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_RSFT,
+        KC_LSFT, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX,       LCTL(LSFT(KC_ESC)),                       WSNAP,        KC_F1, KC_F2,  KC_F3,  KC_F4,  XXXXXXX,
                                             _______,       _______,            _______,     _______, _______,      _______
     ),
 
     [_M_NUM] = LAYOUT(
                  CC_EXLM, CC_AT,   CC_HASH, CC_DLR,  CC_PERC,                       CC_CIRC, CC_AMPR, CC_ASTR, CC_LPRN, CC_RPRN,
                  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_LSFT, KC_GRV,  CC_TILD, CC_PLUS, KC_MINS, CC_ASTR,                       KC_BSLS, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            _______, _______, _______,     _______, _______,      _______
+        KC_LSFT, KC_GRV,  CC_TILD, CC_PLUS, KC_MINS, CC_ASTR,                       KC_BSLS, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+                                            _______, _______, _______,     _______, _______, _______
     ),
 
     [_W_NUM] = LAYOUT(
                  CC_EXLM, CC_AT,   CC_HASH, CC_DLR,  CC_PERC,                       CC_CIRC, CC_AMPR, CC_ASTR, CC_LPRN, CC_RPRN,
                  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_LSFT, KC_GRV,  CC_TILD, CC_PLUS, KC_MINS, CC_ASTR,                       KC_BSLS, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            _______, _______, _______,     _______, _______,      _______
+        KC_LSFT, KC_GRV,  CC_TILD, CC_PLUS, KC_MINS, CC_ASTR,                       KC_BSLS, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+                                            _______, _______, _______,     _______, _______, _______
     ),
 
     /*
